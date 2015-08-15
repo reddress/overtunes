1c724460f049f2105fcac8745fe67b06fca9f1d1
Heitor Chang <heitorchang@gmail.com>
2015-08-15 18:44:53 -0300

    fanfare as chords

--- ff1-fanfare.clj
+++ ff1-fanfare.clj
@@ -5,7 +5,7 @@
 
 ;;;; https://github.com/overtone/overtone/wiki/Pitches-and-Chords
 
-(definst saw-wave [freq 440 attack 0.01 sustain 0.25 release 0.1 vol 0.4]
+(definst saw-wave [freq 440 attack 0.01 sustain 0.21 release 0.1 vol 0.4]
   (* (env-gen (env-lin attack sustain release) 1 1 0 1 FREE)
      (saw freq)
      vol))
@@ -24,25 +24,28 @@
 (defn play-seq [inst intv & notes]
   (map-indexed #(play-note-at (* intv %1) %2 inst) notes))
 
+(comment
 ;;;; awful timing
 (play-seq saw2 190 :A+ :r :G+ :r   :A+ :r :G+ :aC+ :r :aC+ :aC :r  :aC+ :aC :r :aC :A+ :r :G+ :r :G :r :G+ :F)
 (play-seq saw2 190 :G  :r :F  :r   :G  :r :F  :F   :r :F   :D+ :r  :F   :D+ :r :D+ :G :r  :F  :r :D+ :r :F :C+)
 (play-seq saw2 190 :d+ :a+ :d+ :a+ :d+ :a+ :d+ :a+ :c+ :g+ :c+ :g+ :c+ :g+ :c+ :g+ :d+ :a+ :d+ :a+ :d+ :a+ :d+ :a+ :c+ :g+ :c+ :g+ :c+ :g+ :c+ :g+)
+)
 
 (defn play-chord [inst & note-kws]
   (let [letter-map {:zb 47 :c 48 :c+ 49 :d 50 :d+ 51 :e 52 :f 53 :f+ 54 :g  55
                     :g+ 56 :a 57 :a+ 58 :b 59 :C 60 :C+ 61 :D 62 :D+ 63 :E 64
                     :F 65 :F+ 66 :G 67 :G+ 68 :A 69 :A+ 70 :B 71 :r 0
                     :aC 72 :aC+ 73 :aD 74 :aD+ 75}]
-        (doseq [note-kw note-kws]
-          (if (not (= note-kw :r)) (inst (letter-map note-kw))))))
+    (doseq [note-kw note-kws]
+      (if (not (= note-kw :r)) (inst (letter-map note-kw))))))
 
-(play-chord saw2 :c :e :g)
+;;; (play-chord saw2 :c :e :g)
 
 (defn play-chord-at [t inst & note-kws]
   (let [time (now)]
     (at (+ time t) (apply play-chord inst note-kws))))
 
+(comment
 (play-chord-at 0 saw2 :A+ :G :d+)
 (play-chord-at 200 saw2 :r :r :a+)
 (play-chord-at 400 saw2 :G+ :F :d+)
@@ -53,5 +56,25 @@
 (play-chord-at 1400 saw2 :aC+ :F :a+)
 (play-chord-at 1600 saw2 :r :r :d+)
 (play-chord-at 1800 saw2 :aC+ :F :a+)
+)
 
-;;;; transpose strings of notes into chords
+(defn play-voices [inst intv & voices]
+  (map-indexed #(apply play-chord-at (* intv %1) inst %2) (apply map vector voices)))
+
+
+;;; (play-voices saw2 1200 [:c :e :g] [:e :r :b] [:g :r :d])
+
+
+(play-voices saw2 200
+             [:A+ :r :G+ :r   :A+ :r :G+ :aC+ :r :aC+ :aC :r  :aC+ :aC :r :aC :A+ :r :G+ :r :G :r :G+ :F  :r :r :r :r :r :r :r :r
+              :A+ :r :G+ :r   :A+ :r :G+ :aC+ :r :aC+ :aC :r  :aC+ :aC :r :aC :A+ :r :G+ :r :A+ :r :aC+ :aD+  :r :r :r :r :r :r :r :r
+              :A+ :r :G+ :r   :A+ :r :G+ :aC+ :r :aC+ :aC :r  :aC+ :aC :r :aC :A+ :r :G+ :r :G :r :G+ :F  :r :r :r :r :r :r :r :r
+              :A+ :r :G+ :r   :A+ :r :G+ :aC+ :r :aC+ :aC :r  :aC+ :aC :r :aC :A+ :r :G+ :r :A+ :r :aC+ :aD+  :r :r :r :r :r :r :r :r]
+             [:G  :r :F  :r   :G  :r :F  :F   :r :F   :D+ :r  :F   :D+ :r :D+ :G :r  :F  :r :D+ :r :F :C+ :r :r :r :r :r :r :r :r
+              :G  :r :F  :r   :G  :r :F  :F   :r :F   :D+ :r  :F   :D+ :r :D+ :G :r  :F  :r :G :r :A+ :B :r :r :r :r :r :r :r :r
+              :G  :r :F  :r   :G  :r :F  :F   :r :F   :D+ :r  :F   :D+ :r :D+ :G :r  :F  :r :D+ :r :F :C+ :r :r :r :r :r :r :r :r
+              :G  :r :F  :r   :G  :r :F  :F   :r :F   :D+ :r  :F   :D+ :r :D+ :G :r  :F  :r :G :r :A+ :B :r :r :r :r :r :r :r :r]
+             [:d+ :a+ :d+ :a+ :d+ :a+ :d+ :a+ :c+ :g+ :c+ :g+ :c+ :g+ :c+ :g+ :d+ :a+ :d+ :a+ :d+ :a+ :d+ :a+ :c+ :g+ :c+ :g+ :c+ :g+ :c+ :g+
+              :d+ :a+ :d+ :a+ :d+ :a+ :d+ :a+ :c+ :g+ :c+ :g+ :c+ :g+ :c+ :g+ :d+ :a+ :d+ :a+ :d+ :a+ :d+ :a+ :zb :f+ :zb :f+ :zb :f+ :zb :f+
+              :d+ :a+ :d+ :a+ :d+ :a+ :d+ :a+ :c+ :g+ :c+ :g+ :c+ :g+ :c+ :g+ :d+ :a+ :d+ :a+ :d+ :a+ :d+ :a+ :c+ :g+ :c+ :g+ :c+ :g+ :c+ :g+
+              :d+ :a+ :d+ :a+ :d+ :a+ :d+ :a+ :c+ :g+ :c+ :g+ :c+ :g+ :c+ :g+ :d+ :a+ :d+ :a+ :d+ :a+ :d+ :a+ :zb :f+ :zb :f+ :zb :f+ :zb :f+])
